acb61894f879d105f282f7598905ccbc30684d60
FScoward <endlick@gmail.com>
2022-01-07 02:36:00 +0900

    :construction: wip

--- src/main/scala/com/github/fscoward/notion/NotionApiClient.scala
+++ src/main/scala/com/github/fscoward/notion/NotionApiClient.scala
@@ -1,10 +1,17 @@
 package com.github.fscoward.notion
 
-import sttp.client3._
-import sttp.client3.circe._
-import sttp.model._
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
-import com.github.fscoward.notion.users._
+import com.github.fscoward.notion.databases.TitleProperty
+import com.github.fscoward.notion.pages.annotation.NotionAnnotation
+import com.github.fscoward.notion.pages.property.TextObj
+import com.github.fscoward.notion.pages.text.TextPropertyValue
+import sttp.client3.*
+import sttp.client3.circe.*
+import sttp.model.*
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import io.circe.syntax.*
+import com.github.fscoward.notion.users.*
 
 class NotionApiClient {
   val headers = Map(
@@ -44,24 +51,103 @@ class NotionApiClient {
     response
   }
   def createPage(parentDatabaseId: String, properties: Map[String, String]) = {
-    val body = s"""
-      |{
-      |  "parent": {
-      |     "database_id": "${parentDatabaseId}"
-      |  },
-      |  "properties": {
-      |   "Name": {
-      |			"title": [
-      |				{
-      |					"text": {
-      |						"content": "Tuscan Kale"
-      |					}
-      |				}
-      |			]
-      |		}
-      | }
-      |}
-      |""".stripMargin
-    post(NotionApiUri.Page.pages, body)
+
+    /*{
+	"parent": { "database_id": "48f8fee9cd794180bc2fec0398253067" },
+	"properties": {
+		"Name": {
+			"title": [
+				{
+					"text": {
+						"content": "Tuscan Kale"
+					}
+				}
+			]
+		},
+		"Description": {
+			"rich_text": [
+				{
+					"text": {
+						"content": "A dark green leafy vegetable"
+					}
+				}
+			]
+		},
+		"Food group": {
+			"select": {
+				"name": "Vegetable"
+			}
+		},
+		"Price": { "number": 2.5 }
+	},
+	"children": [
+		{
+			"object": "block",
+			"type": "heading_2",
+			"heading_2": {
+				"text": [{ "type": "text", "text": { "content": "Lacinato kale" } }]
+			}
+		},
+		{
+			"object": "block",
+			"type": "paragraph",
+			"paragraph": {
+				"text": [
+					{
+						"type": "text",
+						"text": {
+							"content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+							"link": { "url": "https://en.wikipedia.org/wiki/Lacinato_kale" }
+						}
+					}
+				]
+			}
+		}
+	]
+}*/
+    case class CreatePageParent(database_id: String)
+    case class KeyName(underlying: String)
+    case class PropertyId(underlying: String)
+    case class Text(content: String) {
+      val key = "text"
+    }
+
+    case class TextProperty(propertyId: PropertyId, property: Text)
+    implicit val tpencoder: Encoder[TextProperty] = new Encoder[TextProperty] {
+      override def apply(a: TextProperty): Json =
+        Json.obj(
+          (
+            a.propertyId.underlying,
+            Json.arr(Json.obj((a.property.key, a.property.asJson)))
+          )
+        )
+    }
+
+    case class Y(
+        parent: CreatePageParent,
+        properties: Map[KeyName, TextProperty]
+    )
+
+    implicit val keyNameEncoder: KeyEncoder[KeyName] = new KeyEncoder[KeyName] {
+      override def apply(foo: KeyName): String = foo.underlying
+    }
+    implicit val propertyTypeEncoder: KeyEncoder[PropertyId] =
+      new KeyEncoder[PropertyId] {
+        override def apply(key: PropertyId): String = key.underlying
+      }
+
+    val b = Y(
+      CreatePageParent(s"$parentDatabaseId"),
+      Map(
+        KeyName("Name") -> TextProperty(
+          PropertyId("title"),
+          Text("this is content")
+        )
+      )
+    )
+    println(b.asJson)
+
+    val response = post(NotionApiUri.Page.pages, b.asJson.toString)
+    println(response)
   }
 }
