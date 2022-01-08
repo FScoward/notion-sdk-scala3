6a7cbbaba413b924740b6c5ae9ee464914980d7d
FScoward <endlick@gmail.com>
2022-01-08 20:02:50 +0900

    :recycle: rename

--- src/main/scala/com/github/fscoward/notion/pages/create/block/HeadingBlock.scala
+++ src/main/scala/com/github/fscoward/notion/pages/create/block/HeadingBlock.scala
@@ -1,9 +1,9 @@
 package com.github.fscoward.notion.pages.create.block
 
-trait HeadingObject extends BlockObject {
+trait HeadingBlock extends BlockObject {
   val text: Seq[TextObject]
 }
 
-case class Heading2Object(text: Seq[TextObject]) extends HeadingObject {
+case class Heading2Object(text: Seq[TextObject]) extends HeadingBlock {
   val `type` = "heading_2"
 }
