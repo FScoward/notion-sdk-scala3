c1778cd82be18bc759c6acdbd420afca2a85e7e8
FScoward <endlick@gmail.com>
2022-01-11 00:22:07 +0900

    :construction: database

--- src/test/scala/com/github/fscoward/notion/databases/create/DatabasePropertySpec.scala
+++ src/test/scala/com/github/fscoward/notion/databases/create/DatabasePropertySpec.scala
@@ -2,16 +2,19 @@ package com.github.fscoward.notion.databases.create
 
 import io.circe._, io.circe.parser._
 import io.circe.Encoder, io.circe.syntax._
+import io.circe.generic.auto._
 
 class DatabasePropertySpec extends munit.FunSuite {
   test("create database property") {
-    val actual = Parent("b24dcaf87a2140d0a1d85b393cd2019b")
+    val actual = Database(Parent("b24dcaf87a2140d0a1d85b393cd2019b"))
     val expected = """
-        {
-          "type": "page_id",
-          "page_id": "b24dcaf87a2140d0a1d85b393cd2019b"
-        }
-        """
+{
+   "parent":{
+      "type":"page_id",
+      "page_id":"b24dcaf87a2140d0a1d85b393cd2019b"
+   }
+}
+      """
     assertEquals(actual.asJson, parse(expected).getOrElse(Json.Null))
   }
 }
