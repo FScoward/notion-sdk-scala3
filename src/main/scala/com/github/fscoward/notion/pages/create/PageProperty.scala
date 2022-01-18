fbc5457e1e1a65aaf1408ac7b520bde313fd771a
FScoward <endlick@gmail.com>
2022-01-18 23:14:57 +0900

    :recycle: move encoder

--- src/main/scala/com/github/fscoward/notion/pages/create/PageProperty.scala
+++ src/main/scala/com/github/fscoward/notion/pages/create/PageProperty.scala
@@ -13,32 +13,14 @@ case class PageProperty(
 )
 
 implicit val encoder: Encoder[PageProperty] = new Encoder[PageProperty] {
-  override def apply(a: PageProperty): Json = {
-    val property: Seq[(String, Json)] = {
-      a.properties.value.map { case (key, value) =>
-        value match {
-          case v: TitleProperty =>
-            (key, Json.obj((v.id, Json.arr(v.asJson))))
-          case v: RitchTextProperty =>
-            (key, Json.obj((v.id, Json.arr(v.asJson))))
-          case v: SelectProperty =>
-            (key, v.asJson)
-          case v: NumberProperty =>
-            (key, v.asJson)
-          case v: EmailProperty =>
-            (key, v.asJson)
-          case v: MultiSelectProperty =>
-            (key, v.asJson)
-        }
-      }.toSeq
-    }
+  override def apply(pageProperty: PageProperty): Json = {
     Json.obj(
-      ("parent", a.parent.asJson),
+      ("parent", pageProperty.parent.asJson),
       (
         "properties",
-        Json.obj(property: _*)
+        pageProperty.properties.asJson
       ),
-      ("children", a.children.asJson)
+      ("children", pageProperty.children.asJson)
     )
   }
 }
--- src/main/scala/com/github/fscoward/notion/pages/create/Properties.scala
+++ src/main/scala/com/github/fscoward/notion/pages/create/Properties.scala
@@ -1,5 +1,33 @@
 package com.github.fscoward.notion.pages.create
 
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import io.circe.syntax.*
+
 case class Properties(
     value: Map[String, Property]
 )
+
+implicit val pagePropertiesEncoder: Encoder[Properties] =
+  new Encoder[Properties] {
+    override def apply(properties: Properties): Json = {
+      Json.obj(properties.value.map { case (key, value) =>
+        value match {
+          case v: TitleProperty =>
+            (key, Json.obj((v.id, Json.arr(v.asJson))))
+          case v: RitchTextProperty =>
+            (key, Json.obj((v.id, Json.arr(v.asJson))))
+          case v: SelectProperty =>
+            (key, v.asJson)
+          case v: NumberProperty =>
+            (key, v.asJson)
+          case v: EmailProperty =>
+            (key, v.asJson)
+          case v: MultiSelectProperty =>
+            (key, v.asJson)
+        }
+      }.toSeq: _*)
+
+    }
+  }
