9d606ce9fc75c7bbee903b7ee20762b7721acacb
FScoward <endlick@gmail.com>
2022-01-22 23:47:34 +0900

    :construction: add test

--- src/test/scala/com/github/fscoward/notion/pages/read/RetrieveAPagePropertyItem.scala
+++ src/test/scala/com/github/fscoward/notion/pages/read/RetrieveAPagePropertyItem.scala
@@ -1,7 +1,29 @@
 package com.github.fscoward.notion.pages.read
 
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import io.circe.syntax.*
+
+case class PagePropertyItem(
+    `object`: String = "list",
+    next_cursor: Option[String],
+    has_more: Boolean
+)
+
 class RetrieveAPagePropertyItem extends munit.FunSuite {
   test("decode json") {
-    assert(true)
+    val json =
+      """
+{
+  "object": "list",
+  "next_cursor": null,
+  "has_more": false
+}        
+      """
+
+    val actual = decode[PagePropertyItem](json)
+    val expected = PagePropertyItem(next_cursor = None, has_more = false)
+    assertEquals(actual, Right(expected))
   }
 }
