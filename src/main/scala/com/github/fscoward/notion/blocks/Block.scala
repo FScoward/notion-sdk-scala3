afa7c56baa818591e70520066f91358fd8543f15
FScoward <endlick@gmail.com>
2022-02-03 02:46:02 +0900

    :recycle: move BlockType

--- src/main/scala/com/github/fscoward/notion/blocks/Block.scala
+++ src/main/scala/com/github/fscoward/notion/blocks/Block.scala
@@ -7,25 +7,6 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import scala.util.Try
 
-enum BlockType:
-  case paragraph extends BlockType
-  case bulleted_list_item extends BlockType
-  case numbered_list_item extends BlockType
-  case toggle extends BlockType
-  case to_do extends BlockType
-  case quote extends BlockType
-  case callout extends BlockType
-  case synced_block extends BlockType
-  case template extends BlockType
-  case column extends BlockType
-  case child_page extends BlockType
-  case child_database extends BlockType
-  case header_1 extends BlockType
-  case header_2 extends BlockType
-  case header_3 extends BlockType
-  case table extends BlockType
-end BlockType
-
 case class Block(
     `object`: String = "block",
     id: String,
@@ -40,9 +21,4 @@ val dateTimeFormatter =
   DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 implicit val datetimeDecoder: Decoder[LocalDateTime] =
   Decoder.decodeLocalDateTimeWithFormatter(dateTimeFormatter)
-
-implicit val blockTypeDecoder: Decoder[BlockType] =
-  Decoder.decodeString.emapTry(blockType => {
-    Try(BlockType.valueOf(blockType))
-  })
 implicit val blockDecoder: Decoder[Block] = deriveDecoder[Block]
---
+++ src/main/scala/com/github/fscoward/notion/blocks/BlockType.scala
@@ -0,0 +1,29 @@
+package com.github.fscoward.notion.blocks
+
+import io.circe.Decoder
+
+import scala.util.Try
+
+enum BlockType:
+  case paragraph extends BlockType
+  case bulleted_list_item extends BlockType
+  case numbered_list_item extends BlockType
+  case toggle extends BlockType
+  case to_do extends BlockType
+  case quote extends BlockType
+  case callout extends BlockType
+  case synced_block extends BlockType
+  case template extends BlockType
+  case column extends BlockType
+  case child_page extends BlockType
+  case child_database extends BlockType
+  case header_1 extends BlockType
+  case header_2 extends BlockType
+  case header_3 extends BlockType
+  case table extends BlockType
+end BlockType
+
+implicit val blockTypeDecoder: Decoder[BlockType] =
+  Decoder.decodeString.emapTry(blockType => {
+    Try(BlockType.valueOf(blockType))
+  })
