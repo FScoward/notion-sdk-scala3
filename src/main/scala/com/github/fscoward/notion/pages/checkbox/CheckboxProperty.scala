f50f8f4c29ec5a45c7119f75a8a168dbade00c7d
FScoward <endlick@gmail.com>
2022-02-21 23:38:47 +0900

    :recycle: property type to enum

--- src/main/scala/com/github/fscoward/notion/pages/checkbox/CheckboxProperty.scala
+++ src/main/scala/com/github/fscoward/notion/pages/checkbox/CheckboxProperty.scala
@@ -1,9 +1,9 @@
 package com.github.fscoward.notion.pages.checkbox
 
-import com.github.fscoward.notion.pages.property.Property
+import com.github.fscoward.notion.pages.property.{Property, PropertyType}
 
 case class CheckboxProperty(
     id: String,
-    `type`: String = "checkbox",
+    `type`: PropertyType = PropertyType.checkbox,
     checkbox: Boolean
 ) extends Property
