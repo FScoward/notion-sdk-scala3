0c8b0381bf17147f7e862a185a3c4234c7eb3405
FScoward <endlick@gmail.com>
2022-02-23 00:47:55 +0900

    :construction: date

--- src/main/scala/com/github/fscoward/notion/databases/query/SingleFilterQuery.scala
+++ src/main/scala/com/github/fscoward/notion/databases/query/SingleFilterQuery.scala
@@ -2,6 +2,7 @@ package com.github.fscoward.notion.databases.query
 
 import com.github.fscoward.notion.databases.query.filter.Filter
 import com.github.fscoward.notion.databases.query.filter.checkbox.CheckboxFilter
+import com.github.fscoward.notion.databases.query.filter.date.DateFilter
 import com.github.fscoward.notion.databases.query.filter.number.NumberFilter
 import com.github.fscoward.notion.databases.query.filter.select.SelectFilter
 import com.github.fscoward.notion.databases.query.filter.text.TextFilter
@@ -17,13 +18,14 @@ import com.github.fscoward.notion.databases.query.filter.checkbox.conditionEncod
 import com.github.fscoward.notion.databases.query.filter.text.conditionEncoder
 import com.github.fscoward.notion.databases.query.filter.number.conditionEncoder
 import com.github.fscoward.notion.databases.query.filter.select.conditionEncoder
+import com.github.fscoward.notion.databases.query.filter.date.conditionEncoder
 implicit val filterEncoder: Encoder[Filter] = Encoder.instance {
   case textFilter: TextFilter               => textFilter.asJson
   case numberFilter: NumberFilter           => numberFilter.asJson
   case checkboxFilter: CheckboxFilter       => checkboxFilter.asJson
   case selectFilter: SelectFilter           => selectFilter.asJson
   case multiSelectFilter: MultiSelectFilter => multiSelectFilter.asJson
-  // date
+  case dateFilter: DateFilter               => dateFilter.asJson
   // people
   // files
   // relation
