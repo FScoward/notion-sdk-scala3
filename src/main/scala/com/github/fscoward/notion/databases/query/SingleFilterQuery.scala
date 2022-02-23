839833ca26fe7b08ce67708916150ad7074a7b41
FScoward <endlick@gmail.com>
2022-02-23 12:52:14 +0900

    :construction: files filter

--- src/main/scala/com/github/fscoward/notion/databases/query/SingleFilterQuery.scala
+++ src/main/scala/com/github/fscoward/notion/databases/query/SingleFilterQuery.scala
@@ -3,6 +3,7 @@ package com.github.fscoward.notion.databases.query
 import com.github.fscoward.notion.databases.query.filter.Filter
 import com.github.fscoward.notion.databases.query.filter.checkbox.CheckboxFilter
 import com.github.fscoward.notion.databases.query.filter.date.DateFilter
+import com.github.fscoward.notion.databases.query.filter.file.FileFilter
 import com.github.fscoward.notion.databases.query.filter.number.NumberFilter
 import com.github.fscoward.notion.databases.query.filter.select.SelectFilter
 import com.github.fscoward.notion.databases.query.filter.text.TextFilter
@@ -21,7 +22,7 @@ import com.github.fscoward.notion.databases.query.filter.number.conditionEncoder
 import com.github.fscoward.notion.databases.query.filter.select.conditionEncoder
 import com.github.fscoward.notion.databases.query.filter.date.conditionEncoder
 import com.github.fscoward.notion.databases.query.filter.people.conditionEncoder
-// TODO: files
+import com.github.fscoward.notion.databases.query.filter.file.conditionEncoder
 // TODO: relation
 // TODO: formula
 
@@ -33,7 +34,7 @@ implicit val filterEncoder: Encoder[Filter] = Encoder.instance {
   case multiSelectFilter: MultiSelectFilter => multiSelectFilter.asJson
   case dateFilter: DateFilter               => dateFilter.asJson
   case peopleFilter: PeopleFilter           => peopleFilter.asJson
-  // TODO: files
+  case fileFilter: FileFilter               => fileFilter.asJson
   // TODO: relation
   // TODO: formula
 }
